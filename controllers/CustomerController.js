13d096a0ffb6aedc2524aacdef4b61ec409524ba
David <d.pitarch@outlook.es>
2020-10-17 11:17:30 +0200

    Edit CustomerController.js
    Implement bcrypt to encrypt customer password when sign up in our app.

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -2,6 +2,7 @@
 const CustomerModel = require ('../models/customer');
 const mongoose = require ('mongoose');
 const crypt = require ('bcrypt');
+const saltRounds = 9;
 const fs = require ('fs');
 const { findOne } = require('../models/customer');
 
@@ -12,11 +13,15 @@ const CustomerController = {
     async signup  (req, res) {
         
         try {
+
+            // Encriptado de la contraseña
+            let encryptPass = await crypt.hash (req.body.password, saltRounds)
+
             const newCustomer = await new CustomerModel ({
                 name: req.body.name,
                 surname: req.body.surname,
                 email: req.body.email,
-                password: req.body.password
+                password: encryptPass
             }).save ();
             
             res.status (201).send ({ message: 'Cliente dado de alta con exito.' });
@@ -30,20 +35,27 @@ const CustomerController = {
     async login (req, res) {
 
         try {
+
             const loginCostumer = await CustomerModel.findOne ({
                 email: req.body.email,
-                password: req.body.password
             })
 
             if ( ! loginCostumer ) {
                 res.status (201).send ({ message: 'Las credenciales introducidas no son validas.'})
             }
             else {
-                res.status (201).send ({ message: `Bienvenido de nuevo ${ loginCostumer.name}.` })
+                let passCheck = await crypt.compare (req.body.password, loginCostumer.password);
+
+                if ( passCheck ) {
+                    res.status (201).send ({ message: `Bienvenido de nuevo ${ loginCostumer.name}.` })
+                }
+                else {
+                    res.status (201).send ({ message: 'Las credenciales introducidas no son validas.'})
+                }
             };
         } 
         catch (error) {
-            res.status (500).send ({ message: 'Las credenciales introducidas no son validas', error})
+            res.status (500).send ({ message: 'Se ha producido un error.', error})
         }
     },
 
