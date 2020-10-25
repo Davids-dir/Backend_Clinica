4f7c80e97e808e787fc4c3331cb317af40d2ccf0
David <d.pitarch@outlook.es>
2020-10-25 10:27:05 +0100

    Set PORT to 3001

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const express = require ('express');
 const morgan = require('morgan');
 const app = express ();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const CORS = require ('./middleware/cors');
 
 // Acceso al archivo de configuracion de la DB
