7bf3248acf9837e03e7466ef3de2a30d4349bec6
David <d.pitarch@outlook.es>
2020-10-25 16:20:25 +0100

    Edit CustomerController.js

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -1,81 +1,81 @@
 // Requisitos del archivo y accesos
-const CustomerModel = require ('../models/customer');
-const mongoose = require ('mongoose');
-const crypt = require ('bcrypt');
+const CustomerModel = require('../models/customer');
+const mongoose = require('mongoose');
+const crypt = require('bcrypt');
 const saltRounds = 9;
-const fs = require ('fs');
 const { findOne } = require('../models/customer');
 
 // Creacion del objeto y sus metodos
 const CustomerController = {
 
     // Metodo para Registrar un nuevo cliente en la DB
-    async signup  (req, res) {
-        
+    async signup(req, res) {
+
         try {
-            
+
             // Encriptado de la contraseña
-            let encryptPass = await crypt.hash (req.body.password, saltRounds)
-            
-            const newCustomer = await new CustomerModel ({
+            let encryptPass = await crypt.hash(req.body.password, saltRounds)
+
+            const newCustomer = await CustomerModel.create({
                 name: req.body.name,
                 surname: req.body.surname,
                 email: req.body.email,
                 password: encryptPass
-            }).save ();
-            
-            res.status (201).send ({ message: 'Cliente dado de alta con exito.', newCostumer });
-        } 
+            }).save();
+
+            res.status(201).send({ message: 'Cliente dado de alta con exito.'});
+
+        }
         catch (error) {
-            res.status (500).send ({ message: 'No se ha podido dar de alta al cliente.' });
+            res.status(500).send({ message: 'No se ha podido dar de alta al cliente.' });
         }
     },
 
     // Metodo para realizar LOGIN
-    async login (req, res) {
+    async login(req, res) {
 
         try {
 
-            const loginCostumer = await CustomerModel.findOne ({
+            const loginCostumer = await CustomerModel.findOne({
                 email: req.body.email,
             })
-             
-            if ( ! loginCostumer ) {
-                res.status (201).send ({ message: 'Las credenciales introducidas no son validas.'})
+
+            if (!loginCostumer) {
+                res.status(201).send({ message: 'Las credenciales introducidas no son validas.' })
             }
             else {
-                let passCheck = await crypt.compare (req.body.password, loginCostumer.password);
+                let passCheck = await crypt.compare(req.body.password, loginCostumer.password);
 
-                if ( passCheck ) {
-                    res.status (201).send ({ message: `Bienvenido de nuevo ${ loginCostumer.name}.` })
+                if (passCheck) {
+                    res.status(201).send({ message: `Bienvenido de nuevo ${loginCostumer.name}.` })
                 }
                 else {
-                    res.status (201).send ({ message: 'Las credenciales introducidas no son validas.'})
+                    res.status(201).send({ message: 'Las credenciales introducidas no son validas.' })
                 };
             };
-        } 
+        }
         catch (error) {
-            res.status (500).send ({ message: 'Se ha producido un error.', error})
+            res.status(500).send({ message: 'Se ha producido un error.', error })
         }
     },
 
     // Metodo para realizar LOGOUT de la aplicacion
-    async logout (req, res) {
+    async logout(req, res) {
 
         try {
-            const logoutCostumer = await CustomerModel.findOne ({
+            const logoutCostumer = await CustomerModel.findOne({
                 email: req.params.email
             })
 
-            if ( ! logoutCostumer ) {
-                res.status (201).send ({ message: 'Las credenciales introducidas no son validas.'})
+            if (!logoutCostumer) {
+                res.status(201).send({ message: 'Las credenciales introducidas no son validas.' })
             }
             else {
-                res.status (201).send ({ message: 'La sesion se ha cerrado correctamente.' })
-            }       
+                res.status(201).send({ message: 'La sesion se ha cerrado correctamente.' })
+            }
         }
         catch (error) {
-            res.status (500).send ({ message: 'Se ha producido un error.', error})
+            res.status(500).send({ message: 'Se ha producido un error.', error })
         };
     }
 }
